44e0186d7c4ffb71b4836c2574df4e5d86e56c3a
gignu <gignupg@gmail.com>
2022-12-24 18:52:13 +0100

    Resizable video plus control buttons below video

--- src/App.tsx
+++ src/App.tsx
@@ -48,95 +48,76 @@ function App() {
       <div id="es" className="col s12"></div>
       <div id="de" className="col s12"></div>
       <div className="row">
-        <h1>{content.title[region]}</h1>
+        <h1>Hancock</h1>
       </div>
-      <div className="row">
-        <p className="flow-text col s8 offset-s2">
-          {content.usageInstructions[region]}
-        </p>
+      <div className="iframe-container">
+        <iframe
+          className="iframe"
+          src="https://player.vimeo.com/video/784124019?h=8b1452e6ab&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          frameBorder="0"
+          allowFullScreen
+          title="video"
+        ></iframe>
       </div>
-      <div className="row">
-        <h3>{content.context[region]}</h3>
+
+      <div className="buttons">
+        <button className="button">Prev</button>
+        <button className="button">Rewind</button>
+        <button className="button">Next</button>
       </div>
-      <div className="row">
-        <p className="flow-text col s8 offset-s2">
-          {content.introductoryText[region]}
-        </p>
-      </div>
-      <div className="row">
-        <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
-          <iframe
-            src="https://player.vimeo.com/video/781670633?h=2514b21dd7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-            frameBorder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowFullScreen
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-            }}
-            title="video1.mp4"
-          ></iframe>
-        </div>
-      </div>
-      <div className="row">
-        <ul className="collapsible col s12 m10 offset-m1">
+
+      <ul className="collapsible col s12 m10 offset-m1">
+        <li>
+          <div className="collapsible-header">
+            <i className="material-icons">subtitles</i>
+            {content.subtitles[region]}
+          </div>
+          <div className="collapsible-body">
+            {text[0].subtitles.en.map((subtitle: string) => (
+              <p className="flow-text">{subtitle}</p>
+            ))}
+          </div>
+        </li>
+        {region !== "en" && (
           <li>
             <div className="collapsible-header">
-              <i className="material-icons">subtitles</i>
-              {content.subtitles[region]}
+              <i className="material-icons">g_translate</i>
+              {content.translation[region]}
             </div>
             <div className="collapsible-body">
-              {text[0].subtitles.en.map((subtitle: string) => (
-                <p className="flow-text">{subtitle}</p>
+              {text[0].translation[region].map((translation: string) => (
+                <p className="flow-text">{translation}</p>
               ))}
             </div>
           </li>
-          {region !== "en" && (
-            <li>
-              <div className="collapsible-header">
-                <i className="material-icons">g_translate</i>
-                {content.translation[region]}
-              </div>
-              <div className="collapsible-body">
-                {text[0].translation[region].map((translation: string) => (
-                  <p className="flow-text">{translation}</p>
-                ))}
-              </div>
-            </li>
-          )}
-          {text[0].vocabulary.length > 0 && (
-            <li>
-              <div className="collapsible-header">
-                <i className="material-icons">school</i>
-                {content.vocabulary[region]}
-              </div>
-              <div className="collapsible-body left-align">
-                <table className="striped">
-                  <thead>
+        )}
+        {text[0].vocabulary.length > 0 && (
+          <li>
+            <div className="collapsible-header">
+              <i className="material-icons">school</i>
+              {content.vocabulary[region]}
+            </div>
+            <div className="collapsible-body left-align">
+              <table className="striped">
+                <thead>
+                  <tr>
+                    <th>Phrase</th>
+                    <th>Definition</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {text[0].vocabulary.map(([phrase, definition]: string[]) => (
                     <tr>
-                      <th>Phrase</th>
-                      <th>Definition</th>
+                      <td>{phrase}</td>
+                      <td>{definition}</td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {text[0].vocabulary.map(
-                      ([phrase, definition]: string[]) => (
-                        <tr>
-                          <td>{phrase}</td>
-                          <td>{definition}</td>
-                        </tr>
-                      )
-                    )}
-                  </tbody>
-                </table>
-              </div>
-            </li>
-          )}
-        </ul>
-      </div>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </li>
+        )}
+      </ul>
     </center>
   );
 }
